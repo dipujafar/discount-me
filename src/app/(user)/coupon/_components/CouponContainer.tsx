3bdb4f46a49515d106d6ff0c5b367ea6b2d295cb
dipujafar <dipujafarmr@gmail.com.>
2024-11-17 16:12:15 +0600

    added coupon page

---
+++ src/app/(user)/coupon/_components/CouponContainer.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/ui/container";
+
+const CouponContainer = () => {
+  return (
+    <Container className="page-padding">
+      <h1> Coupon Container</h1>
+    </Container>
+  );
+};
+
+export default CouponContainer;
---
+++ src/app/(user)/coupon/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CouponContainer from "./_components/CouponContainer";
+
+export const metadata = {
+  title: "Coupon",
+  description: "Earn exclusive offers and discounts on your favorite products.",
+};
+
+const CouponPage = () => {
+  return (
+    <div>
+      <CouponContainer></CouponContainer>
+    </div>
+  );
+};
+
+export default CouponPage;
